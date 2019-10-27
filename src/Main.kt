92634c4693de42b392a93e6cf39eca57100fa51e
Alberto Bagnacani <albertobagnacani@gmail.com>
2019-10-27 18:21:48 +0100

    TODO removal

--- src/Main.kt
+++ src/Main.kt
@@ -10,7 +10,6 @@ import model.state.player.NormalPlayer
 import model.state.rules.NormalGameRules
 import model.state.rules.StandardGameRulesFactory
 
-// TODO doc
 fun main(args : Array<String>) {
     val boardTypePath = "resources/normalBoardType.txt"
     val boardContentPath = "resources/normalBoardContent.txt"
@@ -35,12 +34,12 @@ fun main(args : Array<String>) {
 
     while(true) {
         // eval action
-        var game = NormalTablutGame(client.state, initialState as NormalState, StandardGameRulesFactory().createFromGameVersion(gameVersion, client.state) as NormalGameRules, NormalActionResolver())
-        var search = TablutIterativeDeepeningAlphaBetaSearch(game, utilMin, utilMax, seconds, heuristic)
+        val game = NormalTablutGame(client.state, initialState as NormalState, StandardGameRulesFactory().createFromGameVersion(gameVersion, client.state) as NormalGameRules, NormalActionResolver())
+        val search = TablutIterativeDeepeningAlphaBetaSearch(game, utilMin, utilMax, seconds, heuristic)
         //var search = IterativeDeepeningAlphaBetaSearch(game, utilMin, utilMax, seconds)
-        var action = search.makeDecision(client.state)
+        val action = search.makeDecision(client.state)
         //println(action)
-        val metrics = search.metrics
+        //val metrics = search.metrics
         //println(metrics)
         client.write(action)
         client.read() // Read what my action did
--- src/model/state/StandardStateFactory.kt
+++ src/model/state/StandardStateFactory.kt
@@ -8,8 +8,9 @@ class StandardStateFactory : StateFactory {
     override fun createFromGameVersion(version: String, boardTypePath: String, boardContentPath: String): State {
         return when(version){
             "Normal" -> NormalState(NormalBoard<NormalBoardCell>(9, 9, boardTypePath, boardContentPath), NormalPlayer.WHITE) // Il giocatore nello stato iniziale è il bianco
-            // "Brandubh" -> BrandubhState()
-            // ...
+            //"Brandubh" -> BrandubhState()
+            //"Classic" -> ClassicState()
+            //"Modern" -> ModernState()
             else -> throw Exception("State not found")
         }
     }
